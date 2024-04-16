a1eee262991c9490fd8074bfa4bbb0535612514f
Najaf Ali <najaf.balti000@gmail.com>
2024-04-16 21:58:55 +0500

    Changes before changing in layout

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -8,24 +8,24 @@ export default function Reviews() {
             <div className="w-full flex flex-col items-start justify-start gap-12">
                 <h2 className='regular-42'>Reviews</h2>
                 <div className='flex items-center justify-center gap-10'>
-                    <div className='flex flex-col gap-4'>
-                        <div className='flex items-start justify-center gap-8'>
-                            <div className="text flex flex-col gap-4">
+                    <div className='flex flex-col'>
+                        <div className='flex justify-center gap-8'>
+                            <div className="text flex flex-col gap-4 border-r-2 pr-8 border-gray-400">
                                 <p>Stirling Gradys baby photography is simply magical. He has a special talent for capturing those fleeting moments of childhood joy and innocence. Our photos are absolutely stunning and we will treasure them for years to come.</p>
                                 <h4> Emily R.</h4>
                             </div>
-                            <img src={SecimgMain} alt='Girl Smilling at corner'/>
+                            <img src={SecimgMain} alt='Girl Smilling at corner' className='pb-4'/>
                         </div>
-                        <img src={imgMain} alt='Hello World'/>
+                        <img src={imgMain} alt='Hello World' className='border-t-2 pt-4 border-gray-400'/>
                     </div>
-                    <div className='flex flex-col gap-4'>
-                        <img src={imgMain}  />
-                        <div className='flex items-start justify-center gap-8'>
-                            <div className="text flex flex-col gap-4">
+                    <div className='flex flex-col'>
+                        <img src={imgMain}  className='border-b-2 pb-4 border-gray-400'/>
+                        <div className='flex justify-center gap-8'>
+                            <div className="text flex flex-col gap-4 border-r-2 pr-8 border-gray-400 pt-4">
                                 <p>Stirling Gradys baby photography is simply magical. He has a special talent for capturing those fleeting moments of childhood joy and innocence. Our photos are absolutely stunning and we will treasure them for years to come.</p>
                                 <h4> Emily R.</h4>
                             </div>
-                            <img src={SecimgMain} alt='Girl Smilling at corner'/>
+                            <img src={SecimgMain} alt='Girl Smilling at corner' className='pt-4'/>
                         </div>
                     </div>
                 </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,31 +3,33 @@ import { Link } from 'react-router-dom'
 import imgMain from "../images/Rectangle-122.jpg"
 
 export default function Services() {
-  return (
-    <div className="py-12 w-[90%] flex flex-col items-center justify-center">
-            <div className="w-full flex flex-col items-center justify-center gap-12">
+    return (
+        <div className="py-12 w-[90%] flex flex-col items-center justify-center">
+            <div className="w-full flex flex-col items-center justify-center gap-16">
                 <h2 className='regular-42'>Services</h2>
-                <div className="w-full flex items-center gap-10">
-                    <div className="flex flex-col gap-4">
-                        <h4 className="uppercase font-bold">baby photography</h4>
-                        <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
-                        <img src={imgMain} alt='Girl Smilling Face'/>
-                        <Link href="/" className="secondary-btn">Book Now</Link>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h4 className="uppercase font-bold">baby photography</h4>
-                        <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
-                        <img src={imgMain} alt='Girl Smilling Face'/>
-                        <Link href="/" className="secondary-btn">Book Now</Link>
-                    </div>
-                    <div className="flex flex-col gap-4">
-                        <h4 className="uppercase font-bold">baby photography</h4>
-                        <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
-                        <img src={imgMain} alt='Girl Smilling Face'/>
-                        <Link href="/" className="secondary-btn">Book Now</Link>
+                <div className="w-full flex items-center gap-10 border-gray-400 border-y-2">
+                    <div className="flex my-[-30px]">
+                        <div className="flex flex-col gap-4 p-16 border-r-2 border-gray-400">
+                            <h4 className="uppercase font-bold">baby photography</h4>
+                            <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
+                            <img src={imgMain} alt='Girl Smilling Face' />
+                            <Link href="/" className="secondary-btn">Book Now</Link>
+                        </div>
+                        <div className="flex flex-col gap-4 p-16 border-r-2 border-gray-400">
+                            <h4 className="uppercase font-bold">baby photography</h4>
+                            <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
+                            <img src={imgMain} alt='Girl Smilling Face' />
+                            <Link href="/" className="secondary-btn">Book Now</Link>
+                        </div>
+                        <div className="flex flex-col gap-4 p-16">
+                            <h4 className="uppercase font-bold">baby photography</h4>
+                            <p> Capturing the innocence and wonder of your little ones, creating timeless memories you'll cherish forever.</p>
+                            <img src={imgMain} alt='Girl Smilling Face' />
+                            <Link href="/" className="secondary-btn">Book Now</Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-  )
+    )
 }
